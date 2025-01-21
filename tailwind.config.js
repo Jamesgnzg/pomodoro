33205c16e2df306e2815deac191587843cd08f1c
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-01-21 19:01:57 +0800

    add comic neue font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+      fontFamily: {
+        sans: ["Comic Neue", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
